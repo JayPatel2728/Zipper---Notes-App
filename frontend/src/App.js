7d6eca15db8711d745f1cdfca17c83919e331ad7
Jay <jay.patel20032@gmail.com>
2022-09-03 18:05:53 -0400

    created navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header/>
+      <main style={{ minHeight:"93vh" }}>
+
+      </main>
+      <Footer/>
+    </>
   );
 }
 
