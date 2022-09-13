1862286e2a0effbf0534945460512ac6683ac959
Jay <jay.patel20032@gmail.com>
2022-09-13 17:01:03 -0400

    created notes page

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,7 +1,72 @@
-import React from 'react'
+import React from "react";
+import { Accordion, Badge, Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { MainScreen } from "../../components/MainScreen/MainScreen";
+import notes from "./Notes";
 
 export const MyNotes = () => {
+  const deleteHandler = () => {
+    if (window.confirm("Are you Sure?")) {
+    }
+  };
+
   return (
-    <div>MyNotes</div>
-  )
-}
+    <MainScreen title="Welcome Back Jay Patel...">
+      <Link to="createnote">
+        <Button style={{ marginLeft: 10, marginBottom: 6 }} size="lg">
+          Create New Note
+        </Button>
+      </Link>
+      {notes.map((note) => (
+        <Accordion defaultActiveKey={["0"]}>
+          <Accordion.Item eventkey="0">
+            <Card style={{ margin: 10 }}>
+              <Card.Header style={{ display: "flex" }}>
+                <span
+                  style={{
+                    color: "black",
+                    textDecoration: "none",
+                    flex: 1,
+                    cursor: "pointer",
+                    alignSelf: "center",
+                    fontSize: 18,
+                  }}
+                >
+                  <Accordion.Button as={Card.Text} variant="link">
+                    {note.title}
+                  </Accordion.Button>
+                </span>
+                <div>
+                  <Button href={`/note/${note._id}`}>Edit</Button>
+                  <Button
+                    variant="danger"
+                    className="mx-2"
+                    onClick={() => deleteHandler(note._id)}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </Card.Header>
+              <Accordion.Collapse>
+                <Card.Body>
+                  <h4>
+                    <Badge bg="success" text="light">
+                      Category - {note.category}{" "}
+                    </Badge>
+                  </h4>
+
+                  <blockquote className="blockquote mb-0">
+                    <p>{note.content}</p>
+                    <footer className="blockquote-footer">
+                      Creater on - date
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion.Item>
+        </Accordion>
+      ))}
+    </MainScreen>
+  );
+};
