f53a125d359f13366e12f45787f15cbc1c4f68f2
Jay <jay.patel20032@gmail.com>
2022-09-17 01:02:50 -0400

    added error middleware

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const { errorHandler, notFound } = require("./middlewares/errorMiddleware");
 
 const app = express();
-dotenv.config(); //so i can use env variables
+const path = require('path');
+dotenv.config({path: path.resolve(__dirname, '../.env')}); //so i can use env variables
 connectDB();
 app.use(express.json()); //used for postman requests
 
@@ -18,6 +20,10 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.use("/api/users", userRoutes);
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
